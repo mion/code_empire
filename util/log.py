44a794976c730724b41486acca07cb1ba3e076ce
Gabriel Vieira <gluisvieira@gmail.com>
2013-04-29 21:56:06 -0300

    Couple of log additions.

--- util/log.py
+++ util/log.py
@@ -1,2 +1,15 @@
-def log(context, message):
-  print "[{}]\t{}".format(context, message)
+import time
+
+LOGGING = True
+
+def timestamp():
+	return time.strftime("%d %b %Y %H:%M:%S", time.gmtime())
+
+def disable():
+	LOGGING = False
+
+def enable():
+	LOGGING = True
+
+def log(context="GLOBAL", message):
+  print "{}\t[{}] - {}".format(timestamp(), context, message)
