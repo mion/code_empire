09f2eae14f0cef9e55937facd6bf2a461f53244f
Gabriel Vieira <gluisvieira@gmail.com>
2013-05-11 14:12:40 -0300

    Handle 'gather' action and some minor refactoring.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# Code Empire Server
+Code Empire Server
+------------------
 A game to test your programming skills.<br>
 This is the server code.
 
 ## How to play?
 All you need to do is fork this repo and you're automatically playing.
 
-# TODO's
-* Refactor attack.py: put it back in the Creature module
-* Add more UnitTests
+TASK LIST
+------------------
+## High
+[ ] Refactor game.py: use just use one file for communication (instead of opening and closing), Unix-style!
+## Normal
+[ ] Create unit tests for game controller.
+[ ] Create unit tests for world.
+## Low
+[ ] Refactor world.py: create a method for each action and handle the message in a creature controller class.
+## Done :)
+[x] Refactor attack.py: put it back in the Creature module.
+[x] Add more UnitTests.
--- model/world.py
+++ model/world.py
@@ -1,5 +1,6 @@
 from model.fortress import Fortress
 from model.creature import Creature
+from model.resource import Resource
 from model.tilemap import TileMap
 from util.point import Point
 from util.dice import Dice
@@ -22,27 +23,38 @@ class World:
                                'creatures': {}, 
                                'gold': 0}
 
-        #self.players[red_player] = {'fortress': Fortress(red_player, Point(World.MAP_SIZE - 1, World.MAP_SIZE - 1)), 'creatures': {}, 'gold': 0}
-        #self.insert_creature(self.players[red_player]['fortress'])
         self.players[red_player]['fortress'].position = Point(World.MAP_SIZE - 1, World.MAP_SIZE - 1)
-        self.tilemap.set_tile_at(self.players[red_player]['fortress'].position.x, 
-                                 self.players[red_player]['fortress'].position.y, 
-                                 self.players[red_player]['fortress'])
+        self.insert_fortress(self.players[red_player]['fortress'])
+        # self.tilemap.set_tile_at(self.players[red_player]['fortress'].position.x, 
+        #                          self.players[red_player]['fortress'].position.y, 
+        #                          self.players[red_player]['fortress'])
         self.insert_creature(Creature('Peon', red_player, position=Point(World.MAP_SIZE - 1, World.MAP_SIZE - 2)))
         self.insert_creature(Creature('Peon', red_player, position=Point(World.MAP_SIZE - 2, World.MAP_SIZE - 1)))
 
-        #self.players[blue_player] = {'fortress': Fortress(blue_player, Point(0, 0)), 'creatures': {}, 'gold': 0}
-        #self.insert_creature(self.players[blue_player]['fortress'])
         self.players[blue_player]['fortress'].position = Point(0, 0)
-        self.tilemap.set_tile_at(self.players[blue_player]['fortress'].position.x, 
-                                 self.players[blue_player]['fortress'].position.y, 
-                                 self.players[blue_player]['fortress'])
+        self.insert_fortress(self.players[blue_player]['fortress'])
+        # self.tilemap.set_tile_at(self.players[blue_player]['fortress'].position.x, 
+        #                          self.players[blue_player]['fortress'].position.y, 
+        #                          self.players[blue_player]['fortress'])
+
         self.insert_creature(Creature('Peon', blue_player, position=Point(0, 1)))
         self.insert_creature(Creature('Peon', blue_player, position=Point(1, 0)))
 
+    def generate_map(self):
+        """
+        Randomically place fortresses, starting creatures and resources.
+        """
+        return
+
+    def insert_fortress(self, fortress):
+        """
+        Insert a fortress into the world.
+        """
+        self.tilemap.set_tile_at(fortress.position.x, fortress.position.y, fortress)
+
     def insert_creature(self, c):
         """
-        Inserts a creature into the world.
+        Insert a creature into the world.
         """
         self.creatures[c.id] = c
         self.players[c.player]['creatures'][c.id] = c
@@ -50,7 +62,7 @@ class World:
 
     def remove_creature(self, c):
         """
-        Removes a creature from the world.
+        Remove a creature from the world.
         """
         del self.creatures[c.id]
         del self.players[c.player]['creatures'][c.id]
@@ -58,7 +70,7 @@ class World:
 
     def move_creature(self, c, dx, dy):
         """
-        Attempts to move a creature by increasing its x and y.
+        Attempt to move a creature by increasing its x and y.
         """
         to_x = c.position.x + dx
         to_y = c.position.y + dy
@@ -77,7 +89,7 @@ class World:
         return filter(lambda player: len(self.players[player]['creatures']) > 0, self.players.keys())
 
     def winner(self):
-        # TODO: check stale winning/drawing conditions.
+        # TODO: check win/draw special cases.
         # (eg: there are no more creatures and Player 1 has not enough resources)
         survivors = self.standing_players()
         if len(survivors) == 1:
@@ -85,7 +97,7 @@ class World:
         else:
             return None
 
-    def gather_creature_info(self, c):
+    def gather_creature_info(self, c): # TODO: change method name... possible confusion with 'gather' from resource?
         """
         Information available to that creature.
         """
@@ -118,18 +130,29 @@ class World:
 
     def handle_creature_response(self, resp, c):
         """
-        Handles a creature's response to the think method.
+        Handle a creature's response to the think method.
         """
         c.memory = resp['memory']
+
+        # TODO: check for errors (e.g.: 'target' is there but no 'target_x', target class, etc)
+        # TODO: optional 'target_id' or 'target_x' and 'target_y'
 
         if resp['action'] == 'move':
             dx, dy = resp.get('dx', 0), resp.get('dy', 0)
             self.move_creature(c, dx, dy)
+
         elif resp['action'] == 'attack':
             target_x, target_y = resp.get('target_x', 0), resp.get('target_y', 0)
             target_creature = self.tilemap.get_tile_at(target_x, target_y)
             if target_creature.__class__ == Creature:
                 c.attack(target_creature)
+
+        elif resp['action'] == 'gather':
+            resource_x, resource_y = resp.get('resource_x', 0), resp.get('resource_y', 0)
+            target_resource = self.tilemap.get_tile_at(resource_x, resource_y)
+            if target_resource.__class__ == Resource:
+                c.gather(target_resource)
+
         elif resp['action'] == 'wander':
             self.move_creature(c, Dice.roll(2) - 1, Dice.roll(2) - 1)
 
